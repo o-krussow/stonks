beb6773af9f581c42e74357f34b2fb3d8bf90295
Owen Krussow <okrus-dev@tuta.io>
2022-10-15 10:57:46 -0500

    Stuff and things

---
+++ README.md
@@ -0,0 +1,23 @@
+## Main Ideas
+We are looking for strategies, not stocks (to an extent)
+
+Figure out when gains are realized for tax purposes
+
+Use google trends
+
+Consider weekends in trading, big spikes happen a lot here it seems
+
+## Lets say we have a stock ticker, determine name of company from the stock ticker
+- Now check google trends for the name of the company as the key word
+- Check news page on google and find which words pop up a lot, use beautiful soup to scrape
+- It would be nice if somehow we could determine positive or negative sentiment from this result
+
+## Strategies:
+- Find stock that follows some index fund roughly, and if stock does not follow index fund when index fund goes up, buy because it probably will. (Same idea if going down)
+
+- Find best "sell level" (or % up) per stock based on back testing, figure out same for downwards trends, determine where to cut losses.
+
+
+
+
+
---
+++ stonk.py
@@ -0,0 +1,39 @@
+import yfinance as yf  
+import matplotlib.pyplot as plt
+import sys
+
+def highest_percent_up(eod_prices):
+    #this does not work right now
+    max_increase = sys.maxsize*-1
+    top_5 = []
+
+    num_days = len(eod_prices)
+    for price_index in range(num_days):
+        if eod_prices[price_index+1] / eod_prices[price_index] > max_increase:
+            max_increase = eod_prices[price_index+1] / eod_prices[price_index]
+            print("Increase:", max_increase)
+            print("Old price:", eod_prices[price_index])
+            print("New price:", eod_prices[price_index+1])
+            print("Index:", price_index)
+        else:
+            break
+
+
+
+
+
+
+
+
+#Right now, this is only giving us an end of day price for every day, instead of different times of every day.
+data = yf.download('SHLDQ','2017-01-01','2022-01-01')
+
+#Converting pandas dataframe to a list because I don't know how to use pandas at all rn
+#If we want to scale this better we will want to use pandas/numpy so whatever computer we run this on won't scream
+data_to_list = data["Adj Close"].values.tolist()
+rounded_list = [round(price, 4) for price in data_to_list] #rounding the list because we do not need 64 bit precision for where we're going
+print(len(rounded_list))
+
+
+highest_percent_up(rounded_list)
+
