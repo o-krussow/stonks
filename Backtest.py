68349ef6d020eb4fca56d1b4d08c7e4011c37973
Owen Krussow <okrus-dev@tuta.io>
2022-10-15 13:30:04 -0500

    Thinking through how exactly I want strategy function to be able to communicate back with the backtesting class, since the backtesting class is what actually keeps track of things since it's running the entire time for a given strategy.
    I think I've decided that some sort of "return signals" would make sense for the strategy function. Maybe the strategy function even returns a function, a function which the backtesting class will run, whether it's buy/sell/something else?

--- Backtest.py
+++ Backtest.py
@@ -4,6 +4,7 @@ from typing import Callable
 class Backtest:
     def __init__(self, tickers: list, start_date: str, end_date: str, evaluate_strategy: Callable):
         self.tickers = tickers
+        self.capital = 0    #Capital is $ to buy assets, right?
         self.start_date = start_date
         self.end_date = end_date
         self.evaluate_strategy = evaluate_strategy #Strategy function
@@ -13,7 +14,10 @@ class Backtest:
 
         while curr_date != self.end_date:
             for ticker in self.tickers:
-                self.evaluate_strategy(ticker.ticker_name)
+                #call the strategy to determine what to do next
+                #Setup "return signals" so Backtest class can communicate with our strategy function on what it decides to do.
+                #This Backtest class is going to have to keep track of our capital, our quantities of different stocks, etc.
+                self.evaluate_strategy(ticker.ticker_name, ticker.quantity, curr_date)
             
             print(curr_date)
             curr_date = utils.add_day(curr_date)    #Progress one day
--- run.py
+++ run.py
@@ -11,13 +11,13 @@ def read_tickers_from_file(filename):
 
 #This is an example of how we can implement one of our strategies, this function is passed to Backtest to be run further down in this file.
 #Things like determining price histories, market sentiment, etc, need to be done in this function. Backtest just iterates through the tickers and keeps track of how many of each one we have.
-def strategy1(ticker_name):
-    print(ticker_name)
+def strategy1(ticker_name, quantity, curr_date):
+    print(ticker_name, quantity)
 
 
 #nasdaq tickers only for now, for testing-----------
 nasdaq_tickers = read_tickers_from_file("nasdaq_tickers.txt")
-objectified_nasdaq_tickers = [Ticker.Ticker(ticker_name, random.randint(0, 10)) for ticker_name in nasdaq_tickers]  #Making a list of Ticker objects with a random quantity from the original list of ticker strings
+objectified_nasdaq_tickers = [Ticker.Ticker(ticker_name, random.randint(0, 100)) for ticker_name in nasdaq_tickers]  #Making a list of Ticker objects with a random quantity from the original list of ticker strings
 #----------------------------------------------------
 
 #Run the Backtest
