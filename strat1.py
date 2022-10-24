a2fa2e2b4411d80ecfcad30293324c84eda6dff5
Owen Krussow <okrus-dev@tuta.io>
2022-10-24 12:23:33 -0500

    Just need to figure out the best way of implementing get_price_at_time in backtest so it can be done

--- strat1.py
+++ strat1.py
@@ -10,13 +10,13 @@ class ExampleStrat(Strat):  #Strategy1 inherits from Strategy
     def recombobulate(self):
         return random.choice(["buy", "sell", "hold"]), random.randint(0, 5)
 
-    #owen is funnty tho :)
     def passes_sniff_test(self, decision, quantity, price, capital, ticker_quant_in_possession):
         if decision == "buy":
             return (quantity * price) <= capital
         elif decision == "sell":
             return quantity <= ticker_quant_in_possession
-        return True
+        else:
+            return True
 
     #Example of overriding a function in Strat
     def main_strategy(self, ticker, ticker_quant_in_possession, price, date, capital):
@@ -25,5 +25,4 @@ class ExampleStrat(Strat):  #Strategy1 inherits from Strategy
         while (not self.passes_sniff_test(decision, quantity, price, capital, ticker_quant_in_possession)):
             decision, quantity = self.recombobulate()
 
-        
         return (decision, quantity)
